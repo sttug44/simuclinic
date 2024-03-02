30941dd1f11ca096ed90eb21f0d6d2e3192a1130
W@dering <126733492+sttug44@users.noreply.github.com>
2024-03-02 14:56:49 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -17,6 +17,27 @@ function goTo(page) {
 }
 
 var icon = document.querySelector('.icon');
-    icon.addEventListener('click', function() {
-      window.location.href = 'massage.html'; // 替换为你要跳转的页面地址
-    });
+icon.addEventListener('click', function () {
+  window.location.href = 'massage.html'; // 替换为你要跳转的页面地址
+});
+
+function startSlider() {
+  var images = document.querySelectorAll('#slider img');
+  var current = 0;
+
+  function nextImage() {
+    // 准备下一张图片的索引
+    var next = (current + 1) % images.length;
+    images[next].style.opacity = '0'; // 确保下一张图片是透明的
+    images[current].style.opacity = '0'; // 当前图片淡出
+    setTimeout(function () {
+      images[next].style.opacity = '1'; // 下一张图片淡入
+      current = next;
+    }, 10); // 给予足够时间让CSS transition生效
+  }
+
+  // 每隔3秒切换一次图片
+  setInterval(nextImage, 3000);
+}
+
+window.onload = startSlider;
