d1cdfec256f77a9db2656c53632116ff0f811b06
W@dering <126733492+sttug44@users.noreply.github.com>
2024-03-03 20:53:56 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -41,3 +41,60 @@ function startSlider() {
 }
 
 window.onload = startSlider;
+
+document.addEventListener('scroll', () => {
+  // 获取页面的滚动高度
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+  // 计算页面的总高度
+  let totalHeight = document.documentElement.scrollHeight;
+  // 获取视口的高度
+  let viewportHeight = window.innerHeight;
+
+  // 判断是否滚动到底部
+  if (scrollTop + viewportHeight >= totalHeight) {
+    // 模拟回弹效果
+    setTimeout(() => {
+      // 先向下滚动一点距离
+      window.scrollTo(0, totalHeight);
+      setTimeout(() => {
+        // 立即回滚到原来的底部位置
+        window.scrollTo(0, totalHeight - 30); // 这里的30可以根据实际效果调整
+      }, 150); // 这里的延时和距离可以根据需要调整以获得最佳效果
+    }, 100);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+  // 处理圆形按钮点击事件
+  document.getElementById('consultation').addEventListener('click', function () {
+    alert('咨询服务');
+  });
+  document.getElementById('registration').addEventListener('click', function () {
+    window.location.href = 'registration.html'; 
+  });
+  document.getElementById('inquiry').addEventListener('click', function () {
+    alert('问诊服务');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const reminderList = document.querySelector('.reminder-list');
+  
+  reminderList.addEventListener('click', function (e) {
+      if (e.target.tagName === 'LI') {
+          e.target.classList.toggle('completed');
+          if (e.target.classList.contains('completed')) {
+              e.target.innerHTML += " <span class='checkmark'>&#10003;</span>";
+          } else {
+              e.target.removeChild(e.target.querySelector('.checkmark'));
+          }
+      }
+  });
+});
+
+
+
+
+
